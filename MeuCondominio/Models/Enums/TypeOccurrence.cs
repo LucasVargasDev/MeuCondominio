ba330b5a120fbfa307b8e1146b8aa6de747a16dc
Lucas Vargas <lucasvargasdev@gmail.com>
2023-06-13 19:04:19 -0300

    Criação das classes Enums

---
+++ MeuCondominio/Models/Enums/TypeOccurrence.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MeuCondominio.Models.Enums
+{
+    public enum TypeOccurrence
+    {
+        [Display(Name = "Multa")]
+        Fine,
+
+        [Display(Name = "Notificação")]
+        Notification,
+
+        [Display(Name = "Manutenção")]
+        Maintenance
+    }
+}
