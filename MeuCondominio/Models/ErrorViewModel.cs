fd0853e4eb9e75a547397685f3fe7c17187d694a
Lucas Vargas <lucasvargasdev@gmail.com>
2023-06-13 09:43:00 -0300

    Adicionar arquivos de projeto.

---
+++ MeuCondominio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeuCondominio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
