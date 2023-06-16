ab7e7ae2743ca8f454abf59ef93a1cebe3348e62
Lucas Vargas <lucasvargasdev@gmail.com>
2023-06-16 15:06:57 -0300

    Alterado idioma para pt-br

--- MeuCondominio/Models/Apartament.cs
+++ MeuCondominio/Models/Apartament.cs
@@ -1,9 +1,14 @@
-﻿namespace MeuCondominio.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MeuCondominio.Models
 {
     public class Apartment
     {
         public int Id { get; set; }
+        [Display(Name = "Número")]
         public string Number { get; set; }
+        [Display(Name = "Andar")]
         public int Floor { get; set; }
         public int BuildingId { get; set; }
         public Building Building { get; set; }
