71b24e73e3bd5df951fc1e23ef4b85339490a787
Lucas Vargas <lucasvargasdev@gmail.com>
2023-06-23 21:21:06 -0300

    Adicionado Graficos da dashboard

--- MeuCondominio/Controllers/HomeController.cs
+++ MeuCondominio/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using MeuCondominio.Models;
+﻿using MeuCondominio.Data;
+using MeuCondominio.Models;
+using MeuCondominio.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace MeuCondominio.Controllers
@@ -7,20 +10,76 @@ namespace MeuCondominio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var dashboardGraphs = new DashboardGraphsViewModel();
+
+            dashboardGraphs.ApartmentChart = GetApartmentChartData();
+            dashboardGraphs.OccurrenceChart = GetOccurrenceChartData();
+
+            return View(dashboardGraphs);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public ChartModel GetApartmentChartData()
+        {
+            var chartData = _context.Apartment
+                .GroupBy(a => a.Floor)
+                .Select(g => new
+                {
+                    Floor = g.Key,
+                    Count = g.Count()
+                })
+                .OrderBy(a => a.Floor)
+                .ToList();
+
+            var chartLabels = chartData.Select(a => $"Andar {a.Floor}").ToList();
+            var chartValues = chartData.Select(a => a.Count).ToList();
+
+            var chartViewModel = new ChartModel
+            {
+                Labels = chartLabels,
+                Values = chartValues
+            };
+
+            return chartViewModel;
+        }
+
+        public ChartModel GetOccurrenceChartData()
+        {
+            var chartData = _context.Occurrence
+                .Include(o => o.Resident)
+                .GroupBy(o => o.Resident.Name)
+                .Select(g => new
+                {
+                    ResidentName = g.Key,
+                    Count = g.Count()
+                })
+                .OrderByDescending(o => o.Count)
+                .ToList();
+
+            var chartLabels = chartData.Select(o => o.ResidentName).ToList();
+            var chartValues = chartData.Select(o => o.Count).ToList();
+
+            var chartViewModel = new ChartModel
+            {
+                Labels = chartLabels,
+                Values = chartValues
+            };
+
+            return chartViewModel;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ MeuCondominio/Models/ChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MeuCondominio.Models
+{
+    public class ChartModel
+    {
+        public List<string> Labels { get; set; }
+        public List<int> Values { get; set; }
+    }
+}
